cef239dc5e1560277371e1df623205a27486d375
jgray9 <jgray9@umbc.edu>
2024-04-04 13:21:28 -0400

    created classes for everything on the screen

---
+++ elements.py
@@ -0,0 +1,102 @@
+import pygame
+
+class Scene:
+    def __init__(self, elements: list) -> None:
+        self.elements = elements
+    
+    def click(self, mouse_x, mouse_y):
+        for e in self.elements:
+            # if next_scene is None element is not clickable
+            if e.next_scene == None:
+                continue
+            # collidepoint checks if a position is inside a rect
+            if e.contains_point(mouse_x, mouse_y):
+                return e.next_scene
+    
+    def draw(self):
+        for e in self.elements:
+            e.draw()
+
+class SceneElement:
+    def __init__(self, screen, x, y, w, h, next_scene) -> None:
+        self.screen = screen
+        # position of element
+        self.x = x
+        self.y = y
+        # size of element
+        self.w = w
+        self.h = h
+        # scene to transition to when element is clicked
+        # (none by default since most elements wont be clickable)
+        self.next_scene = next_scene
+    
+    def contains_point(self, x, y) -> bool:
+        # too far left
+        if x < self.x: return False
+        # too far right
+        if x > self.x + self.w: return False
+        # too far up
+        if y < self.y: return False
+        # too far down
+        if y > self.y + self.h: return False
+        return True
+    
+    def draw(self):
+        pass
+
+class ImageElement(SceneElement):
+    def __init__(self, screen, x, y, w, h, filename, next_scene=None) -> None:
+        # call super constructor
+        super().__init__(screen, x, y, w, h, next_scene)
+        # load image from file
+        # loading an image automatically creates a surface with the image on it
+        self.image_surface = pygame.image.load(filename)
+        # rescale image to size
+        self.image_surface = pygame.transform.scale(self.image_surface, (w, h))
+    
+    def draw(self):
+        # blit() draws one surface onto another surface
+        self.screen.blit(self.image_surface, (self.x, self.y))
+
+class TextElement(SceneElement):
+    def __init__(self, screen, x, y, w, h, text, text_size, next_scene=None) -> None:
+        # call super constructor
+        super().__init__(screen, x, y, w, h, next_scene)
+        # create new font with size size
+        # no need for 'self.' since this wont be used outside of __init__
+        font = pygame.font.Font(pygame.font.get_default_font(), text_size)
+        # pygame can only render one line at a time
+        # text must be split into multiple surfaces
+        self.text_surfaces = []
+        # iterate through each line in the text
+        for line in text.split('\n'):
+            # create a new surface object with text on it
+            text_surface = font.render(line, True, "black")
+            self.text_surfaces.append(text_surface)
+    
+    def draw(self):
+        curr_y = self.y
+        for text_surface in self.text_surfaces:
+            # blit() draws one surface onto another surface
+            self.screen.blit(text_surface, (self.x, curr_y))
+            # increase y by the text_height for the next line
+            curr_y += text_surface.get_height()
+
+class TitleElement(SceneElement):
+    def __init__(self, screen, x, y, w, h, text, text_size, next_scene='road') -> None:
+        # call super constructor
+        super().__init__(screen, x, y, w, h, next_scene)
+        # create new font with size size
+        # no need for 'self.' since this wont be used outside of __init__
+        font = pygame.font.Font(pygame.font.get_default_font(), text_size)
+        # titles are only one line so one surface should suffice
+        self.text_surface = font.render(text, True, "black")
+    
+    def draw(self):
+        # center text in element
+        # x coordinate specifies left of the object
+        # move text right by element_width / 2 so text_x is at center of element
+        # move text left by text_width / 2 so text is at center of the element
+        centered_x = self.x + (self.w / 2) - (self.text_surface.get_width() / 2)
+        # blit() draws one surface onto another surface
+        self.screen.blit(self.text_surface, (centered_x, self.y))
--- kash.py
+++ kash.py
@@ -1,64 +1,22 @@
 import pygame
-import func
+import elements
 
 WINDOW_SIZE = (800, 600)
 
-pygame.init()
-screen = pygame.display.set_mode(WINDOW_SIZE)
-scene = "ROAD"
-TITLE_FONT = pygame.font.Font(pygame.font.get_default_font(), 30)
-
-class ImageButton:
-    def __init__(self, filename, x, y, w, h, scene):
-        # load image from file
-        self.image = pygame.image.load(filename)
-        # rescale image to size
-        self.image = pygame.transform.scale(self.image, (w, h))
-        # create shape to project image onto
-        self.image_rect = pygame.Rect(x, y, w, h)
-        # scene to transition to after image is clicked
-        self.scene = scene
-        # bool for if image is visible
-        self.visible = False
-
-    def draw(self):
-        # dont draw if not visible
-        if self.visible:
-            # screen.blit adds an element to the screen
-            screen.blit(self.image, self.image_rect)
-
-    def click(self, mouse_pos):
-        # Rect.collidepoint checks if a location is inside the rectangle
-        if self.visible and self.image_rect.collidepoint(mouse_pos):
-            change_scene(self.scene)
-
-images = [
-    ImageButton("pin.png", 200, 400, 86, 150, "VHA"),
-    ImageButton("pin.png", 540, 190, 86, 150, "VBA"),
-    ImageButton("pin.png", 180, 35, 86, 150, "NCA"),
-    ImageButton("back.png", 10, 10, 50, 50, "ROAD")
-]
-
-def change_scene(new_scene):
-    print("new scene = {}".format(new_scene))
-    scene = new_scene
-
-    # Clear screen
-    screen.fill("white")
-
-    # Show road & pins if scene is road
-    for i in images:
-        i.visible = scene == "ROAD"
-    # Show back button only if scene is not road
-    images[3].visible = scene != "ROAD"
-
-    # Default screen
-    if scene == "ROAD":
-        func.draw_image(screen, "road.png", 0, 0, WINDOW_SIZE[0], WINDOW_SIZE[1])
-    # Veterans Health Administration
-    elif scene == "VHA":
-        func.draw_text(screen, "Veterans Health Administration (VHA)", 30, WINDOW_SIZE[0] / 2, 50, True)
-        func.draw_text(screen,
+def create_scenes(screen: pygame.Surface):
+    scenes = {}
+    # create road scene
+    scenes['road'] = elements.Scene([
+        elements.ImageElement(screen, 0, 0, 800, 600, "road.png"),
+        elements.ImageElement(screen, 200, 400, 86, 150, 'pin.png', 'vha'),
+        elements.ImageElement(screen, 540, 190, 86, 150, 'pin.png', 'vba'),
+        elements.ImageElement(screen, 180, 35, 86, 150, 'pin.png', 'nca')
+    ])
+    # create vha scene
+    scenes['vha'] = elements.Scene([
+        elements.TitleElement(screen, 100, 50, 600, 30, "Veterans Health Administration (VHA)", 30),
+        elements.ImageElement(screen, 10, 10, 50, 50, "back.png", "road"),
+        elements.TextElement(screen, 50, 100, 700, 400,
 """The Veterans Health Administration (VHA) provides healthcare
 services to veterans, such as regular checkups with a primary care
 provider or appointments with specialists (cardiologists, gynecologists, etc.)
@@ -66,11 +24,13 @@ Veterans are sorted into 8 priority groups, which determine the cost of
 copayments as well as access to additional benefits such as dental care.
 
 Website: https://www.va.gov/health/
-Phone Number: 1-877-222-VETS (8387)""", 18, 50, 100)
-    # Veterans Benefits Administration
-    elif scene == "VBA":
-        func.draw_text(screen, "Veterans Benefits Administration (VBA)", 30, WINDOW_SIZE[0] / 2, 50, True)
-        func.draw_text(screen,
+Phone Number: 1-877-222-VETS (8387)""", 18)
+    ])
+    # create vba scene
+    scenes['vba'] = elements.Scene([
+        elements.TitleElement(screen, 100, 50, 600, 30, "Veterans Benefits Administration (VBA)", 30),
+        elements.ImageElement(screen, 10, 10, 50, 50, "back.png", "road"),
+        elements.TextElement(screen, 50, 100, 700, 400,
 """The Veterans Benefits Administration (VBA) provides many different
 services to help veterans transition to civilian life. This includes
 assistance with home loans, college tuition, and carrer counseling.
@@ -78,11 +38,13 @@ VBA is also responsible for pensions and monetary compensation for the
 elderly and disabled. 
 
 Website: https://benefits.va.gov/benefits/
-Phone Number: 1-800-827-1000""", 18, 50, 100)
-    # National Cemetery Administration
-    elif scene == "NCA":
-        func.draw_text(screen, "National Cemetery Administration (NCA)", 30, WINDOW_SIZE[0] / 2, 50, True)
-        func.draw_text(screen,
+Phone Number: 1-800-827-1000""", 18)
+    ])
+    # create nca scene
+    scenes['nca'] = elements.Scene([
+        elements.TitleElement(screen, 100, 50, 600, 30, "National Cemetery Administration (NCA)", 30),
+        elements.ImageElement(screen, 10, 10, 50, 50, "back.png", "road"),
+        elements.TextElement(screen, 50, 100, 700, 400,
 """The National Cemetery Administration (NCA) provides burial spaces
 for veterans and maintains national cemetaries. NCA covers the cost
 of the gravesite, grave liner, opening/closing of the grave, and U.S.
@@ -90,29 +52,46 @@ burial flag for free. Spouses, widows, and children of veterans are
 also covered by the NCA, even if they die before the veteran themself.
 
 Website: https://www.cem.va.gov/
-Phone Number: 1-800-698-2411""", 18, 50, 100)
-    # Draw all images
-    for i in images: i.draw()
-    # Update screen
-    pygame.display.flip()
+Phone Number: 1-800-698-2411""", 18)
+    ])
 
-change_scene("ROAD")
-running = True
-while running:
+    return scenes
 
-    events = pygame.event.get() # all events that occured last frame
-    for event in events:
-        # user closes the window
-        if event.type == pygame.QUIT:
-            running = False
-        # user clicks the mouse
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            # coordinates of mouse
-            mouse_pos = pygame.mouse.get_pos()
-            print(mouse_pos)
-            # [1:] starts at index one / road image is a background image and shouldnt be clicked
-            for i in images:
-                i.click(mouse_pos)
+def program_loop(screen, scenes):
+    # start on the road scene
+    current_scene = scenes['road']
+    # loop until program closes
+    while True:
 
+        # HANDLE INPUT
 
-pygame.quit()
+        events = pygame.event.get() # all events that occured last frame
+        for event in events:
+            # user closes the window
+            if event.type == pygame.QUIT:
+                return
+            # user clicks the mouse
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # coordinates of mouse
+                mouse_position = pygame.mouse.get_pos()
+                # Scene.click() returns name of scene to transition to if a clickable element is clicked
+                # click() returns None of no clickable element is clicked
+                next_scene_name = current_scene.click(mouse_position[0], mouse_position[1])
+                current_scene = scenes[next_scene_name] if next_scene_name != None else current_scene
+        
+        # HANDLE OUTPUT
+
+        # Clear screen
+        screen.fill("white")
+        # Draw new elements
+        current_scene.draw()
+        # Update display
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+    scenes = create_scenes(screen)
+    program_loop(screen, scenes)
+
+    pygame.quit()
