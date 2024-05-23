6abea4abb2eb1d1c555686195e646ad4da18b740
jgray9 <jgray9@umbc.edu>
2024-05-23 09:14:07 -0400

    --add-data for text.json and images

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
-pyinstaller kash.py --onefile --distpath .
+pyinstaller --onefile --distpath . ^
+--add-data "text.json:." ^
+--add-data "images\back.png:." ^
+--add-data "images\pin.png:." ^
+--add-data "images\road.png:." ^
+kash.py
 rmdir /s /q build
 del kash.spec
