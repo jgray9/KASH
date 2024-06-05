5c2481630d54624b1f1f0f1a348dc2bea06f3337
jgray9 <jgray9@umbc.edu>
2024-06-05 19:38:54 -0400

    added more comments

--- kash.py
+++ kash.py
@@ -41,7 +41,7 @@ def create_scenes(screen: pygame.Surface):
             elements.ImageElement(screen, get_file_path('images/sign.png'), x = 0, y = y, next_scene = department),
             elements.TitleElement(screen, department, 24, 'white', x = 129, y = y + 10, w = 140),
         ])
-        y += space_between_elements + 40
+        y += space_between_elements + 40    # 40 is height of the sign element
     
     # 
     # create department info screens
@@ -52,7 +52,7 @@ def create_scenes(screen: pygame.Surface):
             elements.ImageElement(screen, get_file_path('images/back.png'), x = 10, y = 10, next_scene='road'),
             elements.TitleElement(screen, department, 30, 'black', x = 0, y = 15, w = WINDOW_SIZE[0]),
             elements.ImageElement(screen, get_file_path('images/search_bar.png'), x = 100, y = 60),
-            elements.TextElement(screen, '', 24, 'black', x = 127, y = 58, w = 1000)
+            elements.TextElement(screen, '', 24, 'black', x = 127, y = 58, w = 1000)    # text for search bar
         ])
 
     return scenes
@@ -80,6 +80,7 @@ def program_loop(screen, scenes):
                     search_text = current_scene.elements[4]
                     # press back key
                     if event.key == pygame.K_BACKSPACE:
+                        # remove last character if text is not empty
                         if len(search_text.text) > 0:
                             search_text.set_text(search_text.text[:-1], search_text.text_size, search_text.text_color)
                     # press enter key
@@ -88,8 +89,9 @@ def program_loop(screen, scenes):
                         search_event = pygame.event.Event(elements.SEARCHARTICLEEVENT, {'search': search_text.text})
                         # trigger the event
                         pygame.event.post(search_event)
-                    # press other key
+                    # press key that is alphanumeric or space bar
                     elif event.unicode.isalnum() or event.unicode == ' ':
+                        # maximum text length is 40
                         if len(search_text.text) < 40:
                             search_text.set_text(search_text.text + event.unicode, search_text.text_size, search_text.text_color)
             # scene changes
@@ -105,16 +107,22 @@ def program_loop(screen, scenes):
         # 
         mouse_x, mouse_y = pygame.mouse.get_pos()
         if current_scene == scenes['road']: # road signs change color when hovered over + info board changes also
+            # iterate through every sign on the road screen
+            # sign background and sign text are different elements, so increment index by 2 each iteration
             for i in range(4,36,2):
-                info_board_title = current_scene.elements[2]
+                # info board elements
+                info_board_title = current_scene.elements[2] # info board title is third element added to road scene during init
                 info_board_text = current_scene.elements[3]
+                # sign background and sign text elements
                 sign = current_scene.elements[i]
-                sign_text = current_scene.elements[i + 1]
+                sign_text = current_scene.elements[i + 1] # sign text element always added after sign background element
+                # mouse is over this sign element, so change image to highlighted version and update info board to the department this sign represents
                 if sign.check_position(mouse_x, mouse_y):
                     sign.set_image(get_file_path('images/sign_highlighted.png'))
                     sign_text.set_text(sign_text.text, sign_text.text_size, 'black')
                     info_board_title.set_text(DEPARTMENTS[sign.next_scene]['title'], info_board_title.text_size, info_board_title.text_color)
                     info_board_text.set_text(DEPARTMENTS[sign.next_scene]['desc'], info_board_text.text_size, info_board_text.text_color)
+                # mouse is not hovering over this sign element, so set image to the normal version
                 else:
                     sign.set_image(get_file_path('images/sign.png'))
                     sign_text.set_text(sign_text.text, sign_text.text_size, 'white')
