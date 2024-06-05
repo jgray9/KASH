38660be14202c7ce1516cd395c8d8377ad5d049c
jgray9 <jgray9@umbc.edu>
2024-06-04 21:39:08 -0400

    text surfaces generated during draw()

--- elements.py
+++ elements.py
@@ -82,40 +82,43 @@ class TextElement(SceneElement):
                  x: int | float, y: int | float,
                  w: int | float,
                  opacity: int = 256) -> None:
+        self.text = text
+        self.text_size = text_size
+        self.text_color = text_color
+        self.x = x
+        self.y = y
+        self.w = w
+        self.opacity = opacity
         # call super constructor
         # width and height are used for click detection, not actual size of display
         # since text elements are never clickable, height does not matter
         # width matters since it is used to determine line breaks
         super().__init__(screen, x, y, w, 0, opacity=opacity)
         self.padding = 5
-        # create new font with size size
-        # no need for 'self.' since this wont be used outside of __init__
-        font = pygame.font.Font(pygame.font.get_default_font(), text_size)
+    
+    def draw(self):
+        # create new font with size text_size
+        font = pygame.font.Font(pygame.font.get_default_font(), self.text_size)
         # pygame can only render one line at a time
-        # text must be split into multiple surfaces
-        self.text_surfaces = []
-        curr_x = x + self.padding
-        curr_y = y + self.padding
+        # text must be rendering using multiple surfaces
+        curr_x = self.x + self.padding
+        curr_y = self.y + self.padding
         # iterate through each word
-        for line in text.split('\n'):
+        for line in self.text.split('\n'):
             for word in line.split(' '):
                 # surface for current word
-                curr_surface = font.render(word + ' ', True, text_color)
+                curr_surface = font.render(word + ' ', True, self.text_color)
                 # if new x is out of bounds, increase y for a new line of text, and reset x
-                if curr_x + curr_surface.get_width() >= x + w - self.padding:
-                    curr_x = x + self.padding
+                if curr_x + curr_surface.get_width() >= self.x + self.w - self.padding:
+                    curr_x = self.x + self.padding
                     curr_y += curr_surface.get_height()
                 # add to list
                 curr_surface.set_alpha(self.opacity)
-                self.text_surfaces.append((curr_x, curr_y, curr_surface))
+                self.screen.blit(curr_surface, (curr_x, curr_y))
                 curr_x += curr_surface.get_width()
             # reset x and increase y for new line
-            curr_x = x + self.padding
+            curr_x = self.x + self.padding
             curr_y += curr_surface.get_height()
-    
-    def draw(self):
-        for x, y, text_surface in self.text_surfaces:
-            self.screen.blit(text_surface, (x, y))
 
 class TitleElement(SceneElement):
     def __init__(self, screen: pygame.Surface,
@@ -123,23 +126,29 @@ class TitleElement(SceneElement):
                  x: int | float, y: int | float,
                  w: int | float,
                  opacity: int = 256) -> None:
+        self.text = text
+        self.text_size = text_size
+        self.text_color = text_color
+        self.x = x
+        self.y = y
+        self.w = w
+        self.opacity = opacity
         # call super constructor
         # width and height are used for click detection, not actual size of display
         # since title elements are never clickable, height does not matter
         # width matters since it is used to center text
         super().__init__(screen, x, y, w, 0, opacity=opacity)
-        # create new font with size size
-        # no need for 'self.' since this wont be used outside of __init__
-        font = pygame.font.Font(pygame.font.get_default_font(), text_size)
-        # titles are only one line so one surface should suffice
-        self.text_surface = font.render(text, True, text_color)
-        self.text_surface.set_alpha(opacity)
     
     def draw(self):
+        # create new font with size text_size
+        font = pygame.font.Font(pygame.font.get_default_font(), self.text_size)
+        # titles are only one line so one surface should suffice
+        text_surface = font.render(self.text, True, self.text_color)
+        text_surface.set_alpha(self.opacity)
         # center text in element
         # text is left aligned by default -> left edge of textbox = left edge of element
         # add element_width / 2 to move the text right -> left edge of text = center of element
         # sub text_width / 2 to move text left -> center of text = center of element
-        centered_x = self.x + (self.w / 2) - (self.text_surface.get_width() / 2)
+        centered_x = self.x + (self.w / 2) - (text_surface.get_width() / 2)
         # blit() draws one surface onto another surface
-        self.screen.blit(self.text_surface, (centered_x, self.y))
+        self.screen.blit(text_surface, (centered_x, self.y))
