8ee31705ea3bc6e5227e9f7c78fa07c27591cecb
jgray9 <jgray9@umbc.edu>
2024-06-04 22:22:45 -0400

    updated to use new json and image files

--- build.bat
+++ build.bat
@@ -1,8 +1,10 @@
 pyinstaller --onefile --distpath . ^
---add-data "text.json:." ^
+--add-data "departments.json:." ^
 --add-data "images\back.png:." ^
 --add-data "images\background.png:." ^
---add-data "images\building.png:." ^
+--add-data "images\background_2.png:." ^
+--add-data "images\info_board.png:." ^
+--add-data "images\sign.png:." ^
 kash.py
 rmdir /s /q build
 del kash.spec
