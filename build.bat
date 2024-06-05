323ec71aa364f7e898a8171a862388f161d2e118
jgray9 <jgray9@umbc.edu>
2024-06-05 15:22:46 -0400

    added highlighted images to build command

--- build.bat
+++ build.bat
@@ -1,10 +1,12 @@
 pyinstaller --onefile --distpath . ^
 --add-data "departments.json:." ^
 --add-data "images\back.png:." ^
+--add-data "images\back_highlighted.png:." ^
 --add-data "images\background.png:." ^
 --add-data "images\background_2.png:." ^
 --add-data "images\info_board.png:." ^
 --add-data "images\sign.png:." ^
+--add-data "images\sign_highlighted.png:." ^
 kash.py
 rmdir /s /q build
 del kash.spec
